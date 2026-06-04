5df07a4ef520cce4b85eb832995251d89b75c21f
Yuki <khongbachofficial@gmail.com>
2026-06-04 12:49:05 +0700

    feat(layout): add MainLayout component with Sidebar and Header

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import { Header } from "./header/Header";
-import { Sidebar } from "./sidebar/Sidebar";
+import { Header } from "../header/Header";
+import { Sidebar } from "../sidebar/Sidebar";
 
 export const MainLayout = () => {
   return (
